8817108a76ae82d24e4b3d5d39dc928386f3b85a
hdytrfli <113211161+hdytrfli@users.noreply.github.com>
2026-06-05 14:27:10 +0700

    chore: update morgan log style to match the other log

--- src/libs/logger.ts
+++ src/libs/logger.ts
@@ -1,3 +1,4 @@
+import type { Request } from 'express';
 import morgan from 'morgan';
 import base from 'pino';
 import pinoHttp from 'pino-http';
@@ -9,7 +10,6 @@ const pino = base({
   level: env.LEVEL,
 });
 
-const morganLogger = morgan('short');
 const pinoLogger = pinoHttp({
   logger: pino,
   transport: {
@@ -19,6 +19,11 @@ const pinoLogger = pinoHttp({
     },
   },
 });
+
+morgan.token('remote-ip', (req: Request) => req.ip);
+morgan.token('method-lower', (req: Request) => req.method.toLowerCase());
+morgan.format('system', '[system] :method-lower :url :status :response-time ms');
+const morganLogger = morgan('system');
 
 export const log = env.NODE_ENV === 'production' ? pino : console;
 export const logger = env.NODE_ENV === 'production' ? pinoLogger : morganLogger;
